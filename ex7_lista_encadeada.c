c09ca8a858bacfbf07fae8f317d4c9374998abb9
maria-raquel <mariaraquelmartinez97@gmail.com>
2022-10-17 15:01:13 -0300

    lista encadeada terminada

--- ex7_lista_encadeada.c
+++ ex7_lista_encadeada.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 // a lista é um ponteiro para o primeiro nó
 // cada nó é do tipo a seguir
 typedef struct no{
@@ -19,7 +18,7 @@ void limpa_lista(No** ll);
 // faz o ponteiro lista ser nulo e libera a memória alocada para cada nó
 
 void mostra_lista(No** ll);
-int menu(No **ll);
+void menu(No **ll);
 
 void insere_inicio(No **ll, int n);
 void insere_fim(No **ll, int n);
@@ -32,6 +31,7 @@ void remove_fim(No **ll);
 int main(){
     No *lista; 
     cria_lista(&lista);
+    menu(&lista);
     return 0;
 }
 
@@ -42,14 +42,14 @@ void cria_lista(No** ll){
 
 void limpa_lista(No** ll){
     No* aux_free = *ll; // vai liberar a memoria de cada nó
-    No* aux_proximo = *ll->prox; // vai salvar o endereço do próximo nó a ser liberado
+    No* aux_proximo = (*ll)->prox; // vai salvar o endereço do próximo nó a ser liberado
     
     *ll = 0;
 
     for(; aux_proximo;){
         free(aux_free);
         aux_free = aux_proximo;
-        aux_proximo = aux_proximo->prox
+        aux_proximo = aux_proximo->prox;
     }
 
     free(aux_free);
@@ -61,6 +61,11 @@ void insere_inicio(No**ll, int n){
     // criando o novo nó
     No* novo;
     novo = (No*) malloc(sizeof(No));
+    
+    if(!novo){
+        puts("\nNão iserido: memória insuficiente!\n");
+        return;
+    }
 
     // preenchendo o novo nó
     novo->dado = n;
@@ -68,14 +73,24 @@ void insere_inicio(No**ll, int n){
 
     // inserindo o novo nó na lista;
     *ll = novo;
+
+    puts("\nInserido!\n");
 }
 
 void insere_fim(No**ll, int n){
-    // ll é o endereço da lista, ou seja, é um ponteiro para um ponteiro para o primeiro nó
+    if(!(*ll)){
+        insere_inicio(ll, n);
+        return;
+    }
 
     // criando o novo nó
     No* novo;
     novo = (No*) malloc(sizeof(No));
+    
+    if(!novo){
+        puts("\nNão iserido: memória insuficiente!\n");
+        return;
+    }
 
     // preenchendo o novo nó
     novo->dado = n;
@@ -84,11 +99,18 @@ void insere_fim(No**ll, int n){
     // inserindo na lista
     No* aux = *ll;
     for (; aux->prox; aux = aux->prox);
-
+    
     aux->prox = novo;
+
+    puts("\nInserido!\n");
 }
 
 void remove_inicio(No **ll){
+    if(!(*ll)){
+        puts("\nLista vazia!\n");
+        return;
+    }
+
     No* aux = *ll;
 
     // removendo da lista
@@ -96,28 +118,46 @@ void remove_inicio(No **ll){
 
     // excluindo o elemento
     free(aux);
+
+    puts("\nRemovido!\n");
 }
 
 void remove_fim(No **ll){
+    if(!(*ll)){
+        puts("\nLista vazia\n");
+        return;
+    }
+
+    if(!(*ll)->prox){
+        remove_inicio(ll);
+        return;
+    }
+    
     No* aux = *ll;
     for(; aux->prox->prox; aux = aux->prox);
     // parando no elemento anterior ao que será removido
 
     free(aux->prox);
     aux->prox = NULL;
+    puts("\nRemovido!\n");
 }
 
 void mostra_lista(No** ll){
+    if (!(*ll)){
+        puts("(vazia)");
+        return;
+    }
+
     printf("(");
 
     No* aux = *ll;
     for(; aux->prox; aux = aux->prox)
         printf("%d, ", aux->dado);
 
-    prinf("%d)\n\n", aux->dado);
+    printf("%d)\n", aux->dado);
 }
 
-int menu(No **ll){
+void menu(No **ll){
     puts("Digite o que deseja fazer:");
     puts("1: mostrar a lista");
     puts("2: inserir elemento no início da lista");
@@ -130,44 +170,38 @@ int menu(No **ll){
     int escolha;
     scanf("%d", &escolha);
 
+    int n = 0;
+
     switch (escolha) {
-        case 1: puts("\nSua lista: ")
+        case 1: puts("\nSua lista: ");
                 mostra_lista(ll);
+                putchar('\n');
                 break;
 
         case 2: puts("Que elemento deseja inserir? ");
-                int n;
                 scanf("%d", &n);
                 insere_inicio(ll, n);
-                puts("inserido!");
                 break;
 
         case 3: puts("Que elemento deseja inserir? ");
-                int n;
                 scanf("%d", &n);
                 insere_fim(ll, n);
-                puts("inserido!");
                 break;
 
         case 4: remove_inicio(ll);
-                puts("removido!");
                 break;        
         
         case 5: remove_fim(ll);
-                puts("removido!");
                 break;
 
         case 6: limpa_lista(ll);
-                puts("lista limpa!");
+                puts("lista limpa!\n");
                 break;
 
-        case -1: return 0;
+        case -1: return;
 
-        default: puts("Valor invalido!");
+        default: puts("\nValor invalido! Selecione um número entre 1 e 6.\n");
                 break;
         }
-
     menu(ll);
-
     }
-    
