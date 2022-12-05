b89176a34befa75062a2e0564e67b48c10413050
maria-raquel <mariaraquelmartinez97@gmail.com>
2022-12-05 17:49:35 -0300

    função de inserção e impressões

--- ex15_arvore_avl.c
+++ ex15_arvore_avl.c
@@ -14,18 +14,22 @@ typedef struct No {
 
 int altura(No* a);
 No* balanceia(No* a);
-No* cria_arvore(No* a);
+No* cria_arvore();
 int fator_de_balanceamento(No* a);
+void imprime_ordenado(No* a);
+void imprime_prefix(No* a);
+No* insere_no(No* a, int n);
+void menu(No* a);
 No* rotaciona_direita(No* a);
 No* rotaciona_esquerda(No* a);
 
-No* insere_no(No* a, int n);
 No* remove_no(No* a, int n);
 void imprime_ordenado(No* a);
-void imprime_linearmente(No* a);
 int altura(No* a);
 
 int main(){
+    No* arvore = cria_arvore();
+    menu(arvore);
     return 0;
 }
 
@@ -62,13 +66,82 @@ No* balanceia(No* a){
     return a;
 }
 
-No* cria_arvore(No* a){
+No* cria_arvore(){
     return NULL;
 }
 
 int fator_de_balanceamento(No* a){
     if (!a) return 0;
     return altura(a->e) - altura(a->d);
+}
+
+void imprime_ordenado(No* a){
+    if(!a) return;
+    imprime_ordenado(a->e);
+    printf("%d ", a->dado);
+    imprime_ordenado(a->d);
+}
+
+void imprime_prefix(No* a){ 
+    putchar('<');
+    if (a){
+        printf("%d", a->dado); 
+        imprime_prefix(a->e); 
+        imprime_prefix(a->d); 
+    }
+    putchar('>');
+}
+
+No* insere_no(No* a, int n){
+    if (!a){
+        No* novo = (No*) malloc(sizeof(No));
+        if (!novo) return NULL;
+        novo->d = novo->e = NULL;
+        novo->dado = n;
+        a = novo;
+    }
+
+    else if (n < a->dado)
+        a->e = insere_no(a->e, n);
+    else 
+        a->d = insere_no(a->d, n);
+    
+    return balanceia(a);
+}
+
+void menu(No* a){
+    int escolha, n;
+
+    do{
+        puts("O que deseja fazer com a árvore?");
+        puts("1: inserir um número");
+        puts("2: imprimir ordenadamente");
+        puts("3: imprimir prefix");
+        puts("-1: encerrar");
+        scanf("%d", &escolha);
+
+        switch (escolha){
+        case 1:
+            puts("Que número?");
+            scanf("%d", &n);
+            a = insere_no(a, n);
+            break;
+        case 2: 
+            putchar('\n');
+            imprime_ordenado(a);
+            printf("\n\n");
+            break;
+        case 3:
+            putchar('\n');
+            imprime_prefix(a);
+            printf("\n\n");
+            break;
+        case -1:
+            return;
+        default:
+            break;
+        }
+    } while (escolha != -1);
 }
 
 No* rotaciona_direita(No* a){
