dbfb545f850aee65114cf6e2117dae86cf9e91b8
maria-raquel <mariaraquelmartinez97@gmail.com>
2022-12-05 13:32:54 -0300

    mais assinaturas, implementação da função altura

--- ex15_arvore_avl.c
+++ ex15_arvore_avl.c
@@ -12,13 +12,33 @@ typedef struct No {
     int dado;
 } No;
 
+int altura(No* a);
+No* cria_arvore(No* a);
+
 No* insere_no(No* a, int n);
 No* remove_no(No* a, int n);
-No* cria_arvore(No* a);
 void imprime_ordenado(No* a);
 void imprime_linearmente(No* a);
+int altura(No* a);
+No* rotaciona_esquerda(No* a);
+No* rotaciona_direita(No* a);
+No* balanceia(No* a);
 
 int main(){
     return 0;
 }
 
+int altura(No* a){
+    if (!a) return 0;
+
+    int altura_e, altura_d;
+    altura_e = altura(a->e);
+    altura_d = altura(a->d);
+
+    if (altura_e > altura_d) return altura_e + 1;
+    else return altura_d + 1;
+}
+
+No* cria_arvore(No* a){
+    return NULL;
+}
