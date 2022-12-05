54d9846d09a353b71abee16702745413a6e202b8
maria-raquel <mariaraquelmartinez97@gmail.com>
2022-12-05 16:52:53 -0300

    função balanceia

--- ex15_arvore_avl.c
+++ ex15_arvore_avl.c
@@ -15,6 +15,7 @@ typedef struct No {
 int altura(No* a);
 No* balanceia(No* a);
 No* cria_arvore(No* a);
+int fator_de_balanceamento(No* a);
 No* rotaciona_direita(No* a);
 No* rotaciona_esquerda(No* a);
 
@@ -39,8 +40,35 @@ int altura(No* a){
     else return altura_d + 1;
 }
 
+No* balanceia(No* a){
+    int fb = fator_de_balanceamento(a);
+
+    // Considerando que a árvore é verificada a cada inserção e remoção,
+    // o fator de balanceamento nunca fica maior que módulo de 2
+    // e o fator de balanceamento dos filhos nunca ficam maior que módulo de 1
+
+    if (fb == -2){
+        if (fator_de_balanceamento(a->d) == 1) 
+            a->d = rotaciona_direita(a->d); // se for 1
+        a = rotaciona_esquerda(a); // se for 1 ou -1
+    }
+
+    else if (fb == 2){
+        if (fator_de_balanceamento(a->e) == -1) 
+            a->e = rotaciona_esquerda(a->e); // se for -1
+        a = rotaciona_direita(a); // se for -1 ou 1
+    }
+
+    return a;
+}
+
 No* cria_arvore(No* a){
     return NULL;
+}
+
+int fator_de_balanceamento(No* a){
+    if (!a) return 0;
+    return altura(a->e) - altura(a->d);
 }
 
 No* rotaciona_direita(No* a){
